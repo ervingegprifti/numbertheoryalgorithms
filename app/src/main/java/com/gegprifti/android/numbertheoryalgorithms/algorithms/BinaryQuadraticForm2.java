fa13993d776785b47c306e799b8d5e4e3d4d8534
Ervin Gegprifti <gegprifti.ervin@gmail.com>
2025-09-19 08:20:05 +0200

    Changed from abcd to abcdef (#37)

--- app/src/main/java/com/gegprifti/android/numbertheoryalgorithms/algorithms/BinaryQuadraticForm2.java
+++ app/src/main/java/com/gegprifti/android/numbertheoryalgorithms/algorithms/BinaryQuadraticForm2.java
@@ -28,18 +28,20 @@ public class BinaryQuadraticForm2 extends Algorithm implements GridCalculator {
             BigInteger b = algorithmParameters.getInput2();
             BigInteger c = algorithmParameters.getInput3();
             BigInteger d = algorithmParameters.getInput4();
+            BigInteger e = algorithmParameters.getInput5();
+            BigInteger f = algorithmParameters.getInput6();
 
             List<List<RowItem>> rows = new ArrayList<>();
 
             BigInteger minX = ZERO;
-            BigInteger maxX = d.abs();
-            if (b.compareTo(ZERO) != 0) {
-                maxX = d.divide(b).add(ONE);
+            BigInteger maxX = f.abs();
+            if (d.compareTo(ZERO) != 0) {
+                maxX = f.divide(d).add(ONE);
             }
             BigInteger minY = ZERO;
-            BigInteger maxY = d.abs();
-            if (c.compareTo(ZERO) != 0) {
-                maxY = d.divide(c).add(ONE);
+            BigInteger maxY = f.abs();
+            if (e.compareTo(ZERO) != 0) {
+                maxY = f.divide(e).add(ONE);
             }
 
             // Create row header.
@@ -49,6 +51,7 @@ public class BinaryQuadraticForm2 extends Algorithm implements GridCalculator {
             //
             for (BigInteger x = minX; x.compareTo(maxX) <= 0; x = x.add(ONE)) {
                 AlgorithmHelper.checkIfCanceled();
+
                 headerRow.add(new RowItem(true, "x=" + x, false));
             }
             rows.add(headerRow);
@@ -62,26 +65,28 @@ public class BinaryQuadraticForm2 extends Algorithm implements GridCalculator {
                 // boolean isYPrime = y.isProbablePrime(10);
                 row.add(new RowItem(true, "y=" + y, false)); // isYPrime
 
-                // Add d values.
+                // Add f values.
                 for(BigInteger x = minX; x.compareTo(maxX) <= 0; x = x.add(ONE)) {
                     AlgorithmHelper.checkIfCanceled();
 
-                    BigInteger axy = a.multiply(x).multiply(y);
-                    BigInteger bx = b.multiply(x);
-                    BigInteger cy = c.multiply(y);
-                    BigInteger n = axy.add(bx).add(cy);
-                    // boolean isNPrime = n.isProbablePrime(10);
+                    BigInteger ax2 = a.multiply(x.pow(2));
+                    BigInteger bxy = b.multiply(x).multiply(y);
+                    BigInteger cy2 = c.multiply(y.pow(2));
+                    BigInteger dx = d.multiply(x);
+                    BigInteger ey = e.multiply(y);
+                    BigInteger fCalculated = ax2.add(bxy).add(cy2).add(dx).add(ey);
+                    // boolean isFPrime = fCalculated.isProbablePrime(10);
 
-                    if (n.equals(d)) {
+                    if (fCalculated.equals(f)) {
                         RowItem rowItemXSolution = headerRow.get(x.intValue() + 1);
                         rowItemXSolution.setHeaderStyle(RowItem.HeaderStyle.HIGHLIGHTED);
                         RowItem rowItemYSolution = row.get(0);
                         rowItemYSolution.setHeaderStyle(RowItem.HeaderStyle.HIGHLIGHTED);
-                        RowItem rowItem = new RowItem(false, n.toString(), false, RowItem.ValueStyle.ORANGE); // isNPrime
+                        RowItem rowItem = new RowItem(false, fCalculated.toString(), false, RowItem.ValueStyle.ORANGE); // isFPrime
                         rowItem.setIsSelected(true);
                         row.add(rowItem);
                     } else {
-                        row.add(new RowItem(false, n.toString(), false)); // isNPrime
+                        row.add(new RowItem(false, fCalculated.toString(), false)); // isFPrime
                     }
                 }
                 rows.add(row);
